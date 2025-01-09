92ddf6d1e3f44f34eb0397d3929151d71509c01f
KushalRegmi61 <kushalbro82@gmail.com>
2025-01-09 09:22:15 +0545

    run the app if it is accessed from main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from ui import PdfToAudiobook
 
 # creating the object of the PdfToAudiobook class
-app = PdfToAudiobook() # creating the object of the PdfToAudiobook class
+
+if __name__ == "__main__":
+    app = PdfToAudiobook()  # creating the object of the PdfToAudiobook class
